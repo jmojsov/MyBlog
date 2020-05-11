07d5d2a18fc95ee1d070e0456bd623c5ab5334a5
Jordan <jordan.mojsov@rocketmail.com>
2020-05-11 16:29:26 +0200

    added new stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
 
 namespace MyBlog.Controllers
 {
@@ -11,7 +12,26 @@ namespace MyBlog.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var post1 = new Post()
+            {
+                Title = "Day 1",
+                ImageUrl = "https://media.daysoftheyear.com/20171223113230/look-up-at-the-sky-day-1.jpg",
+                Description = "the sky is very blue"
+            };
+
+            var post2 = new Post()
+            {
+                Title = "Day 2",
+                ImageUrl = "https://www.lovethispic.com/uploaded_images/121991-Golden-Sunny-Day.jpg",
+                Description = "old tree"
+            };
+
+            var posts = new List<Post>();
+
+            posts.Add(post1);
+            posts.Add(post2);
+
+            return View(posts);
         }
 
         
---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+
+    }
+}
